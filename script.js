40f9e083f8d5a64237d9f114781c2f721111776d
Saybabano <sayba22@navgurukul.org>
2024-03-27 10:34:41 +0530

    initial

---
+++ script.js
@@ -0,0 +1,68 @@
+// 1. Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.
+let a=20;
+let b=30;
+console.log(a+b)
+console.log(a*b)
+console.log(a%b)
+
+// 2. Take two numbers and only print the integer part when a is divided by b.
+// let a=80;
+// let b=40;
+// console.log(a/b)
+// 3. Take two numbers a,b and print the exponential power of the first number raised by the second.
+// let a=30;
+// let b=10;
+// console.log(a**b)
+// 4. Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+// let a=10;
+// let b=60;
+// console.log((a%b)/b)
+// 5. Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+// let a=30;
+// let b=25;
+// let c=40;
+// let d=3;
+// let e=6;
+// console.log((a+b+c+d+e)/5)
+// 6. Take the cost price a and selling price b of a product, and print the profit obtained on the product.
+// let cost=90;
+// let price=50;
+// let profit=cost-price;
+// console.log(profit)
+
+// 7. Take a number and print out the last digit of the number.
+// let a=2345667;
+// console.log(a%10)
+// 8. Take a 2 digit number and return the first and last digits. 
+// let a=45;
+// let last=a%10
+// let first=parseInt(a/10)
+// console.log(first,last)
+
+// 9. Write a JavaScript program to find the largest of three numbers.
+// let a=30;
+// let b=40;
+// let c=70;
+// if(( a>b ) && (a>c)){
+//     console.log("A")
+// }
+// else if (( b>a )&& (b>c)){
+//     console.log("B")
+// }
+// else{
+//     console.log("C")
+// }
+// 10. Write a JavaScript program to check whether a number is even or odd.
+// let a=30;
+// if (a%2==0){
+//     console.log("Even")
+// }
+// else{
+//     console.log("Odd")
+// }
+// 11. Write a JavaScript program to check whether a given number is prime or not.
+
+// 12. Write a JavaScript program to convert temperature from Celsius to Fahrenheit and vice versa.
+// let f=32;
+// console.log((f * 9/5) + 32)
+
